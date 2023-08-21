77ff1539f519ea0c482afa8d19490575296f6ad6
Jame-peace03 <thanhbinh290603@gmail.com>
2023-08-21 14:18:24 +0700

    first commit

---
+++ QuanLySieuThi/ExportExecl.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Office.Interop.Excel;
+using System.Data;
+using System.Data.SqlClient;
+using app = Microsoft.Office.Interop.Excel.Application;
+using System.Windows.Forms;
+using Application = System.Windows.Forms.Application;
+using System.IO;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+namespace QuanLySieuThi
+{
+    class ExportExecl
+    {
+        public static string duongdanex = Application.StartupPath;
+        /*public static void exportecxel(DataGridView g, string duongdan, string tenfile)
+        {
+            duongdan = duongdanex + @"\taikhoan\";
+            app obj = new app();
+            obj.Application.Workbooks.Add(Type.Missing);
+            obj.Columns.ColumnWidth = 25;
+            for (int i = 1; i < g.Columns.Count + 1; i++)
+            {
+                obj.Cells[1, i] = g.Columns[i - 1].HeaderText;
+            }
+            for (int i = 0; i < g.Rows.Count; i++)
+                for (int j = 0; j < g.Columns.Count; j++)
+                {
+                    if (g.Rows[i].Cells[j].Value != null)
+                    {
+                        obj.Cells[i + 2, j + 1] = g.Rows[i].Cells[j].Value;
+                    }
+                }
+            obj.Range["A1", "M100"].Font.Name = "Times New Roman";
+            obj.Range["A1", "M100"].HorizontalAlignment = 3;
+            obj.ActiveWorkbook.SaveCopyAs(duongdan + tenfile + ".xlsx");
+            obj.ActiveWorkbook.Saved = true;
+            //obj.Quit();
+        }
+        */
+
+        public static void exportecxelchitietdonhang(DataGridView g, string duongdan, string tenfile)
+        {
+            duongdan = duongdanex + @"\ChiTietDonHang\";
+            app obj = new app();
+            obj.Application.Workbooks.Add(Type.Missing);
+            obj.Columns.ColumnWidth = 25;
+            for (int i = 1; i < g.Columns.Count + 1; i++)
+            {
+                obj.Cells[1, i] = g.Columns[i - 1].HeaderText;
+            }
+            for (int i = 0; i < g.Rows.Count; i++)
+                for (int j = 0; j < g.Columns.Count; j++)
+                {
+                    if (g.Rows[i].Cells[j].Value != null)
+                    {
+                        obj.Cells[i + 2, j + 1] = g.Rows[i].Cells[j].Value;
+                    }
+                }
+            obj.Range["A1", "M100"].Font.Name = "Times New Roman";
+            obj.Range["A1", "M100"].HorizontalAlignment = 3;
+            obj.ActiveWorkbook.SaveCopyAs(duongdan + tenfile + ".xlsx");
+            obj.ActiveWorkbook.Saved = true;
+            //obj.Quit();
+        }
+        public static void ExportToPDF(DataGridView g, string duongdan, string tenfile)
+        {
+            // Đường dẫn đến thư mục lưu trữ file PDF
+            duongdan = duongdanex + @"\ChiTietDonHang-PDF\";
+
+            // Tạo một tài liệu PDF mới
+            Document doc = new Document();
+            PdfWriter writer = PdfWriter.GetInstance(doc, new FileStream(duongdan + tenfile + ".pdf", FileMode.Create));
+
+            // Mở tài liệu để bắt đầu viết
+            doc.Open();
+
+            // Tạo một phần tử tiêu đề cho tài liệu PDF
+            Paragraph title = new Paragraph("Thông tin don hàng", new iTextSharp.text.Font(iTextSharp.text.Font.FontFamily.TIMES_ROMAN, 18f, iTextSharp.text.Font.BOLD));
+            title.Alignment = Element.ALIGN_CENTER;
+            doc.Add(title);
+
+            // Thêm một đoạn trống (space)
+            Paragraph space = new Paragraph(" ");
+            doc.Add(space);
+
+            PdfPTable pdfTable = new PdfPTable(g.Columns.Count);
+            pdfTable.DefaultCell.Padding = 3;
+            pdfTable.WidthPercentage = 100;
+            pdfTable.HorizontalAlignment = Element.ALIGN_LEFT;
+
+            // Thêm tiêu đề từng cột vào tệp PDF
+            for (int i = 0; i < g.Columns.Count; i++)
+            {
+                PdfPCell cell = new PdfPCell(new Phrase(g.Columns[i].HeaderText, new iTextSharp.text.Font(iTextSharp.text.Font.FontFamily.TIMES_ROMAN, 12f, iTextSharp.text.Font.BOLD)));
+                pdfTable.AddCell(cell);
+            }
+
+            // Thêm dữ liệu từ DataGridView vào tệp PDF
+            for (int i = 0; i < g.Rows.Count; i++)
+            {
+                for (int j = 0; j < g.Columns.Count; j++)
+                {
+                    if (g.Rows[i].Cells[j].Value != null)
+                    {
+                        pdfTable.AddCell(g.Rows[i].Cells[j].Value.ToString());
+                    }
+                    else
+                    {
+                        pdfTable.AddCell("");
+                    }
+                }
+            }
+
+            // Thêm bảng vào tài liệu PDF
+            doc.Add(pdfTable);
+
+            // Đóng tài liệu
+            doc.Close();
+            writer.Close();
+        }
+
+        /* public static void export_phieu(DataGridView g, string duongdan, string tenfile, string solg)
+         {
+             duongdan = duongdanex + @"\ThongTinPhieu\";
+             app obj = new app();
+             obj.Application.Workbooks.Add(Type.Missing);
+             obj.Columns.ColumnWidth = 25;
+
+
+             for (int i = 1; i < g.Columns.Count + 1; i++)
+             {
+                 obj.Cells[1, i] = g.Columns[i - 1].HeaderText;
+             }
+             for (int i = 0; i < g.Rows.Count; i++)
+                 for (int j = 0; j < g.Columns.Count; j++)
+                 {
+                     if (g.Rows[i].Cells[j].Value != null)
+                     {
+                         obj.Cells[i + 2, j + 1] = g.Rows[i].Cells[j].Value;
+                     }
+                 }
+             obj.Cells[g.Rows.Count + 2, g.Columns.Count - 1] = "Số lượng : ";
+             obj.Cells[g.Rows.Count + 2, g.Columns.Count] = " " + solg + "";
+
+             obj.Range["A1", "M100"].Font.Name = "Times New Roman";
+             obj.Range["A1", "M100"].HorizontalAlignment = 3;
+             obj.ActiveWorkbook.SaveCopyAs(duongdan + tenfile + ".xlsx");
+             obj.ActiveWorkbook.Saved = true;
+             //obj.Quit();
+         }
+        */
+        public static void nhapnhieu(DataGridView g, string duongdan, string tenfile, string s, string tile)
+        {
+            duongdan = duongdanex + @"\NhapNhieu\";
+            app obj = new app();
+            obj.Application.Workbooks.Add(Type.Missing);
+            obj.Columns.ColumnWidth = 25;
+
+ 
+            for (int i = 1; i < g.Columns.Count + 1; i++)
+            {
+                obj.Cells[1, i] = g.Columns[i - 1].HeaderText;
+            }
+            for (int i = 0; i < g.Rows.Count; i++)
+                for (int j = 0; j < g.Columns.Count; j++)
+                {
+                    if (g.Rows[i].Cells[j].Value != null)
+                    {
+                        obj.Cells[i + 2, j + 1] = g.Rows[i].Cells[j].Value;
+                    }
+                }
+            //obj.Cells[g.Rows.Count + 2, g.Columns.Count - 1] = "Chiếu khấu : ";
+            //obj.Cells[g.Rows.Count + 2, g.Columns.Count] = " " + chietkhau + " %";
+            obj.Cells[g.Rows.Count + 3, g.Columns.Count - 1] = "Tổng Tiền : ";
+            obj.Cells[g.Rows.Count + 3, g.Columns.Count] = " " + s;
+            obj.Range["A1", "M100"].Font.Name = "Times New Roman";
+            obj.Range["A1", "M100"].HorizontalAlignment = 3;
+            obj.ActiveWorkbook.SaveCopyAs(duongdan + tenfile + ".xlsx");
+            obj.ActiveWorkbook.Saved = true;
+            //obj.Quit();
+        }
+    }
+}
---
+++ QuanLySieuThi/GUI/main1.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace QuanLySieuThi
+{
+    partial class main1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(main1));
+            this.label6 = new System.Windows.Forms.Label();
+            this.lbform = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.đăngNhậpHệThốngToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.thôngTinToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.thoátToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.đăngKíToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbsv = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label6
+            // 
+            this.label6.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.label6.Location = new System.Drawing.Point(0, 123);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(947, 12);
+            this.label6.TabIndex = 37;
+            // 
+            // lbform
+            // 
+            this.lbform.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.lbform.BackColor = System.Drawing.Color.Transparent;
+            this.lbform.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbform.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.lbform.Location = new System.Drawing.Point(296, 59);
+            this.lbform.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbform.Name = "lbform";
+            this.lbform.Size = new System.Drawing.Size(461, 64);
+            this.lbform.TabIndex = 36;
+            this.lbform.Text = "Quản Lý Siêu Thị";
+            this.lbform.Click += new System.EventHandler(this.lbform_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.đăngNhậpHệThốngToolStripMenuItem,
+            this.thôngTinToolStripMenuItem,
+            this.thoátToolStripMenuItem,
+            this.đăngKíToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(934, 28);
+            this.menuStrip1.TabIndex = 43;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // đăngNhậpHệThốngToolStripMenuItem
+            // 
+            this.đăngNhậpHệThốngToolStripMenuItem.Name = "đăngNhậpHệThốngToolStripMenuItem";
+            this.đăngNhậpHệThốngToolStripMenuItem.Size = new System.Drawing.Size(165, 24);
+            this.đăngNhậpHệThốngToolStripMenuItem.Text = "Đăng Nhập Hệ thống";
+            this.đăngNhậpHệThốngToolStripMenuItem.Click += new System.EventHandler(this.đăngNhậpHệThốngToolStripMenuItem_Click);
+            // 
+            // thôngTinToolStripMenuItem
+            // 
+            this.thôngTinToolStripMenuItem.Name = "thôngTinToolStripMenuItem";
+            this.thôngTinToolStripMenuItem.Size = new System.Drawing.Size(89, 24);
+            this.thôngTinToolStripMenuItem.Text = "Thông Tin";
+            this.thôngTinToolStripMenuItem.Click += new System.EventHandler(this.thôngTinToolStripMenuItem_Click);
+            // 
+            // thoátToolStripMenuItem
+            // 
+            this.thoátToolStripMenuItem.Name = "thoátToolStripMenuItem";
+            this.thoátToolStripMenuItem.Size = new System.Drawing.Size(61, 24);
+            this.thoátToolStripMenuItem.Text = "Thoát";
+            this.thoátToolStripMenuItem.Click += new System.EventHandler(this.thoátToolStripMenuItem_Click);
+            // 
+            // đăngKíToolStripMenuItem
+            // 
+            this.đăngKíToolStripMenuItem.Name = "đăngKíToolStripMenuItem";
+            this.đăngKíToolStripMenuItem.Size = new System.Drawing.Size(144, 24);
+            this.đăngKíToolStripMenuItem.Text = "Đăng Kí Tài Khoản";
+            this.đăngKíToolStripMenuItem.Click += new System.EventHandler(this.đăngKíToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(233, 148);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(524, 77);
+            this.label1.TabIndex = 44;
+            this.label1.Text = "TRƯỜNG CAO ĐẲNG CÔNG THƯƠNG TP.HCM \r\nKHOA CÔNG NGHỆ THÔNG TIN\r\n";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lbsv
+            // 
+            this.lbsv.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.lbsv.AutoSize = true;
+            this.lbsv.BackColor = System.Drawing.Color.Transparent;
+            this.lbsv.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbsv.Location = new System.Drawing.Point(275, 462);
+            this.lbsv.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbsv.Name = "lbsv";
+            this.lbsv.Size = new System.Drawing.Size(425, 36);
+            this.lbsv.TabIndex = 38;
+            this.lbsv.Text = "Người Thực Hiện : Thanh Bình ";
+            this.lbsv.Click += new System.EventHandler(this.lbsv_Click);
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.label4.Location = new System.Drawing.Point(299, 429);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(371, 33);
+            this.label4.TabIndex = 39;
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.ErrorImage = null;
+            this.pictureBox1.Image = global::QuanLySieuThi.Properties.Resources.download;
+            this.pictureBox1.InitialImage = null;
+            this.pictureBox1.Location = new System.Drawing.Point(359, 214);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(248, 222);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 45;
+            this.pictureBox1.TabStop = false;
+            // 
+            // main1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(934, 575);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lbsv);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.lbform);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "main1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Quản Lý siêu thị";
+            this.Load += new System.EventHandler(this.main1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lbform;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem đăngNhậpHệThốngToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem thôngTinToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem thoátToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ToolStripMenuItem đăngKíToolStripMenuItem;
+        private System.Windows.Forms.Label lbsv;
+        private System.Windows.Forms.Label label4;
+    }
+}
